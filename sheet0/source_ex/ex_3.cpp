ef59569a5b4c16b37c751b04fead0da5cd6afda2
Yanick Kind <yanick.kind@udo.edu>
2023-04-10 13:39:15 +0200

    lol

--- sheet0/source_ex/ex_3.cpp
+++ sheet0/source_ex/ex_3.cpp
@@ -1,30 +1,63 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-vector<double> values;
+//vector<double> euler(int steps, double y0){
+//
+//}
+//
+//vector<double> symm_euler(int steps, double y0, double y1){
+//
+//}
 
-int main () {
-        double steps = 1000.0;
-        double val = 0, temp = 0;
-    for(int n = 0; n < steps; n++){
-        if(n == 0){
-            temp = 1;
-        }
-        val = temp*(1 - 10/steps);
-        temp = val;
-        values.push_back(val);
-    }
-    ofstream symm ("source_ex/output/ex_3_symm.txt");
-    if (symm.is_open())
+vector<double> normal_values, symm_values;
+
+int main()
+{
+    double steps = 150.0;
+    double val = 1;
+    double temp = val;
+    
+    for (int n = 0; n < steps; n++)
     {
-        for(int i = 0; i < values.size(); i++){
-            symm << values[i] << endl;
-        }
-        symm.close();
+        normal_values.push_back(val);
+        val = temp * (1 - 10 / steps);
+        temp = val;
     }
-    else cout << "Unable to open file";
 
-  return 0;
+    double temp_2 = 1, new_temp_2 = 1;
+    temp = exp(-10/steps);
+    for (int n = 0; n < steps; n++)
+    {
+        if(n == 0){
+            symm_values.push_back(temp_2);
+            continue;
+        }
+        if (n == 1)
+        {
+            symm_values.push_back(exp(-10/steps));
+            temp_2 = 1;
+            temp = exp(-10/steps);
+            continue;   
+        }
+        val = -2 * 10/steps * temp + temp_2;
+        temp_2 = temp;
+        temp = val;
+        symm_values.push_back(val);
+    }
+    ofstream euler("source_ex/output/ex_3_euler.txt");
+    if (euler.is_open())
+    {
+        for (int i = 0; i < normal_values.size(); i++)
+        {
+            euler << normal_values[i] << "\t" << symm_values[i] << endl;
+        }
+        euler.close();
+    }
+    else
+        cout << "Unable to open file";
+
+    return 0;
 }
--- sheet0/source_ex/ex_3_plot.py
+++ sheet0/source_ex/ex_3_plot.py
@@ -1,11 +1,23 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-values = np.genfromtxt('source_ex/output/ex_3_symm.txt', unpack = True)
+normal_values, symm_values = np.genfromtxt('source_ex/output/ex_3_euler.txt', unpack = True)
+t_plot = np.linspace(0, 10, normal_values.size)
 
-t_plot = np.linspace(0, 10, values.size)
-
-plt.plot(t_plot, values, label = "Euler-Verfahren")
+plt.subplot(2,1,1)
+plt.plot(t_plot, normal_values, label = "Euler-Verfahren")
 plt.plot(t_plot, np.exp(-t_plot), label = "Exaktes Ergebnis")
+
+plt.ylabel('y')
+plt.legend()
+
+plt.subplot(2,1,2)
+plt.plot(t_plot, symm_values, label = "Symmetrisches Euler-Verfahren")
+plt.plot(t_plot, np.exp(-t_plot), label = "Exaktes Ergebnis")
+
+plt.xlabel('t')
+plt.ylabel('y')
+
+
 plt.legend()
 plt.savefig("build/ex_3_plot_symm.pdf", bbox_inches='tight', pad_inches=0)
