76bdc7e4832efc546151e0ca389643a16bda3d2e
Yanick Kind <yanick.kind@udo.edu>
2023-04-16 14:13:29 +0200

    1a angefangen

--- TEMPLATE/source/ex_1.cpp
+++ TEMPLATE/source/ex_1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main () {
-  ofstream myfile ("source_ex/output/ex_1.txt");
+  ofstream myfile ("source/output/ex_1.txt");
   if (myfile.is_open())
   {
     myfile << "Hello World";
--- sheet1/source/ex_1.cpp
+++ sheet1/source/ex_1.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
+
+
+void vierpunkt(vector <double>& derivative, vector<double>& func, int max_steps){
+  for(int i = 1; i < max_steps; i += 2)
+  derivative.push_back(
+    (func[i] - func[i-1])/(i/max_steps)
+  );
+}
+
 int main () {
-  ofstream myfile ("source_ex/output/ex_1.txt");
+  //part a)
+
+  double x = 0; // x-value where the derivative will be calculated
+  double max_steps = 1000.0; //step size
+  vector<double> derivative;
+  vector<double> f1;
+  for(int i = 1; i<max_steps; i++){
+    f1.push_back(sin(x+i/max_steps));
+    f1.push_back(sin(x-i/max_steps));
+  }
+  
+vierpunkt(derivative, f1, max_steps);
+
+
+
+  ofstream myfile ("source/output/ex_1a.txt");
   if (myfile.is_open())
   {
-    myfile << "Hello World";
-    myfile.close();
+    for(int i = 1; i < derivative.size(); i++){
+      myfile << i/max_steps << "\t" << derivative[i] << endl;
+    } 
+
   }
   else cout << "Unable to open file";
   return 0;
