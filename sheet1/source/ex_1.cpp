5f4dfad57666ac2568189d2739e63e563ffa9189
Yanick Kind <yanick.kind@udo.edu>
2023-04-17 22:27:24 +0200

    lol

--- sheet1/source/ex_1.cpp
+++ sheet1/source/ex_1.cpp
@@ -2,42 +2,94 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <numbers>
 using namespace std;
 
 
-
-void vierpunkt(vector <double>& derivative, vector<double>& func, double max_steps){
-  for(int i = 0; i < max_steps; i += 2){
-  //cout << "TOP: " << func[i] << "\tBOT: " << func[i-1] << "\tstep: " << i/max_steps << endl;
-  derivative.push_back((func[i+1] - func[i])/((i+1)/max_steps));
-  }
+void zweipunkt(vector <float>& deriv, float upper, float lower, float step_size){
+    deriv.push_back((upper - lower)/(2 * step_size));
 }
 
 int main () {
   //part a)
  
-  double x = 0; // x-value where the derivative will be calculated
-  double max_steps = 1000.0; //step size
-  vector<double> derivative;
-  vector<double> f1;
+  float x = 0; // x-value where the num_deriv will be calculated
+  float max_steps = 1000.0; // number of steps
+  vector<float> num_deriv;
+  vector<float> f1; //  in depence on the step size in this half
   for(int i = 0; i<max_steps; i++){
     f1.push_back(sin(x-(i+1)/max_steps));
     f1.push_back(sin(x+(i+1)/max_steps));
   }
-  
-vierpunkt(derivative, f1, max_steps);
+
+  for(int i = 0; i < max_steps; i += 2){
+    zweipunkt(num_deriv, f1[i+1], f1[i], (i+1)/max_steps);
+  }
 
   ofstream myfile ("source/output/ex_1a.txt");
   if (myfile.is_open())
   {
-    for(int i = 0; i < derivative.size(); i++){
-      myfile << (i+1)/max_steps << "\t" << derivative[i] << endl;
+    for(int i = 0; i < num_deriv.size(); i++){
+      myfile << (i+1)/max_steps << "\t" << num_deriv[i] << endl;
     } 
 
   }
   else cout << "Unable to open file";
 
- double h = 0.144; // fixed step size for second half of part a)
+  float h = 0.3; // fixed step size for second half of part a)
 
+  num_deriv.clear();
+  f1.clear(); // for second half in depence on x
+  vector<float> ana_deriv; //analytical results
+
+  double num_points = max_steps; // just for readability
+  for(int i = 0; i < num_points; i++){
+    f1.push_back(sin((-numbers::pi + 2 * numbers::pi * i/num_points - h )));
+    f1.push_back(sin((-numbers::pi + 2 * numbers::pi * i/num_points + h )));
+  }
+
+  for(int i = 0; i < num_points; i += 2){
+    zweipunkt(num_deriv, f1[i+1], f1[i], h);
+  }
+
+  for(int i = 0; i < num_points; i++){
+    ana_deriv.push_back(cos(-numbers::pi + 2 * numbers::pi * i/num_points));
+  }
+
+  ofstream error ("source/output/ex_1a_error.txt");
+  if (error.is_open())
+  {
+    error << "x\t" << "numerical\t" << "analytical\t" << "relative_error" << endl;
+    for(int i = 0; i < num_deriv.size(); i++){
+      error << -numbers::pi + 2 * numbers::pi * i/num_points << "\t" << num_deriv[i] << "\t"
+      << ana_deriv[i] << "\t" << abs((num_deriv[i] - ana_deriv[i])/ana_deriv[i]) << endl;
+    } 
+
+  }
+  else cout << "Unable to open file";
+
+  //part b)
+
+  vector<float> num_sec_deriv;
+  vector<float> ana_sec_deriv;
+  for(int i = 0; i < num_points; i += 2){
+    zweipunkt(num_sec_deriv, num_deriv[i+1], num_deriv[i], h);
+  }
+
+  for(int i = 0; i < num_points; i++){
+    ana_sec_deriv.push_back(-sin(-numbers::pi + 2 * numbers::pi * i/num_points));
+  }
+
+  ofstream sec_deriv ("source/output/ex_1b  .txt");
+  if (sec_deriv.is_open())
+  {
+    sec_deriv << "x\t" << "numerical\t" << "analytical\t" << "relative_error" << endl;
+    for(int i = 0; i < num_deriv.size(); i++){
+      sec_deriv << -numbers::pi + 2 * numbers::pi * i/num_points << "\t" << num_sec_deriv[i] << "\t"
+      << ana_deriv[i] << "\t" << abs((num_sec_deriv[i] - ana_sec_deriv[i])/ana_sec_deriv[i]) << endl;
+    } 
+
+  }
+  else cout << "Unable to open file";
   return 0;
 }
--- sheet1/source/ex_1.py
+++ sheet1/source/ex_1.py
@@ -2,8 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 step_size, derivative = np.genfromtxt('source/output/ex_1a.txt', unpack = True)
+x, num_deriv, ana_deriv, rel_error = np.genfromtxt('source/output/ex_1a_error.txt', unpack = True)
 
+
+plt.figure(1)
 plt.plot(step_size, derivative)
-
+plt.xlabel('h')
+plt.ylabel(r'$f_1^{\prime}(x)$')
+plt.legend()
 plt.tight_layout()
-plt.savefig("build/ex_1.pdf")  
+plt.savefig('build/ex_1a_varstep.pdf') 
+
+plt.figure(2)
+plt.plot(x, num_deriv, label = 'Numerisch')
+plt.plot(x, ana_deriv, label = 'Analytisch')
+plt.xlabel('x')
+plt.ylabel(r'$f_1^{\prime} (x)$')
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/ex_1a_compare.pdf')
+
+plt.figure(3)
+plt.plot(x, rel_error)
+plt.xlabel('x')
+plt.ylabel(r'$\symup{\Delta} f_{1, \text{Zweipunkt}}^{\prime} $')
+plt.tight_layout()
+plt.savefig('build/ex_1a_error.pdf')
