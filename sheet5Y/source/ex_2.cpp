09ffe6a4a29b35f1af55695b07f5f6e13766a759
Yanick Kind <yanick.kind@udo.edu>
2023-05-20 13:45:47 +0200

    Aufgabe 2 nur noch die e fertig machen

---
+++ sheet5Y/source/ex_2.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+const double a = 1, b = 1.5, dx = 1e-3, dy = 1e-3, dt = 1e-5; // dimensions of the membrane and the spacial/time steps
+const int maxTime = 1000, Nx = a/dx, Ny = b/dy; // number of steps in time and spacial directions
+
+void iterationScheme(vector<vector<double>> u, vector<double> init){
+    
+}
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
